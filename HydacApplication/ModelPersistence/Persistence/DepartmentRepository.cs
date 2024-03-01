5372a77260a1dfd7564629ba1fb488f6ba90aafb
ealbrechtsen <emilmalikalbrechtsen@gmail.com>
2024-03-01 21:30:20 +0100

    Connected json file to database. Added NuGet related using directives to Repos

--- HydacApplication/ModelPersistence/Persistence/DepartmentRepository.cs
+++ HydacApplication/ModelPersistence/Persistence/DepartmentRepository.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using Microsoft.Extensions.Configuration.Json;
+
 
 
 namespace ModelPersistence.Persistence
