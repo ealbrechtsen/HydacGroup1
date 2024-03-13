99e9f65a80dc1682d359a0cc4c49615b87688fab
ealbrechtsen <emilmalikalbrechtsen@gmail.com>
2024-03-13 11:26:58 +0100

    Completed Threads51 exercise 6.

---
+++ Threads51_01/Threads51_01/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Threads51_01
+{
+    class Program
+    {
+        private static object _myLock = new object();
+        private char _sharedChar;
+        private const int SIMULATE_WORK = 100;
+
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Run();
+        }
+
+
+        public void Run()
+        {
+            Thread tA = new Thread(WriteA);
+            Thread tB = new Thread(WriteB);
+            tA.Name = "Thread A";
+            tB.Name = "Thread B";
+            tA.Start();
+            tB.Start();
+            tA.Join();
+            tB.Join();
+            Console.Write("\nPress a key ....");
+            Console.ReadKey();
+        }
+
+
+        private void WriteA()
+        {
+            lock (_myLock)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _sharedChar = 'A';
+                    Thread.Sleep(SIMULATE_WORK);
+                    Console.WriteLine($"{Thread.CurrentThread.Name} : {_sharedChar}");
+                    Thread.Yield();
+                }
+            }
+        }
+
+
+        private void WriteB()
+        {
+            lock (_myLock)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _sharedChar = 'B';
+                    Thread.Sleep(SIMULATE_WORK);
+                    Console.WriteLine($"{Thread.CurrentThread.Name} : {_sharedChar}");
+                    Thread.Yield();
+                }
+            }
+
+        }
+    }
+
+}
