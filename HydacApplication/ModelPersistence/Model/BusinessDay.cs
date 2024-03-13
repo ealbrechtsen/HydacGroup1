7e34fa9d35d35f9afbdf5c1722ff5dfb9374a761
Klunz1 <Klunz@live.dk>
2024-03-13 12:58:30 +0100

    Vi har prøvet noget med keychip

---
+++ HydacApplication/ModelPersistence/Model/BusinessDay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelPersistence.Model
+{
+    public class BusinessDay
+    {
+        public DateOnly Date { get; set; }
+
+        public BusinessDay(DateOnly date)
+        {
+            Date = date;
+        }
+    }
+}
---
+++ HydacApplication/ModelPersistence/Model/TimePeriod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelPersistence.Model
+{
+    public class TimePeriod
+    {
+        public int TimePeriodId { get; set; }
+        public TimeOnly CheckInTime { get; set; }
+        public TimeOnly? CheckOutTime { get; set; }
+        public BusinessDay BusinessDay { get; set; }
+
+        public TimePeriod(TimeOnly checkInTime, TimeOnly checkOutTime, BusinessDay businessDay)
+        {
+            CheckInTime = checkInTime;
+            CheckOutTime = checkOutTime;
+            BusinessDay = businessDay;
+        }
+    }
+}
